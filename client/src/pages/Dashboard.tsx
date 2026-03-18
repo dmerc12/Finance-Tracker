a15b26b816099e101102afa0f4eebd690125db11
dmerc12 <dylanmercer12@icloud.com>
2026-03-18 11:33:05 -0500

    feat(frontend): add React Hook Form for future form handling

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import PlaceholderForm from '../components/PlaceholderForm';
 
 const Dashboard: React.FC = () => {
     return (
         <div>
             <h1>Dashboard Page</h1>
             <button className="btn btn-primary">Test Bootstrap</button>
+            <PlaceholderForm />
         </div>
     );
 };
