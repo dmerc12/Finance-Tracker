b6cb5df97e874a15c17d5d8fbc135d217eb415bf
dmerc12 <dylanmercer12@icloud.com>
2026-04-03 12:06:47 -0500

    feat(data): add mock categories, accounts, and transaction data

---
+++ client/src/data/index.ts
@@ -0,0 +1,15 @@
+export type { Transaction, SimpleTransaction } from './mockTransactions';
+export { mockAccounts, mockAccountsSimple } from './mockAccounts';
+export type { Account } from './mockAccounts';
+export {
+    mockTransactions,
+    mockRecentTransactions,
+    mockAccountTransactions,
+} from './mockTransactions';
+export {
+    incomeCategories,
+    expenseCategories,
+    allCategories,
+    transferCategory,
+    getCategoriesForType,
+} from './categories';
