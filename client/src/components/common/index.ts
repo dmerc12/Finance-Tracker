cf724254c43b0fe79b5ed0ea06b8883f9b567d6c
dmerc12 <dylanmercer12@icloud.com>
2026-04-04 15:13:31 -0500

    fix(ui): move components only used in dashboard out of common

--- client/src/components/common/index.ts
+++ client/src/components/common/index.ts
@@ -1,17 +1,4 @@
-import AccountSummaryCard from '../dashboard/AccountSummaryCard.tsx';
 import ConfirmDialog from './ConfirmDialog';
-import LoadingState from './LoadingState';
-import EmptyState from './EmptyState';
 import FormDialog from './FormDialog';
-import StatsGrid from '../dashboard/StatsGrid.tsx';
-import StatCard from '../dashboard/StatCard.tsx';
 
-export {
-    ConfirmDialog,
-    FormDialog,
-    AccountSummaryCard,
-    StatCard,
-    StatsGrid,
-    LoadingState,
-    EmptyState,
-};
+export { ConfirmDialog, FormDialog };
