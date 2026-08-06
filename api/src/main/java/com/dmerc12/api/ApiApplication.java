3c26f224a6db00089e059ad7882a8b2951d3d471
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2026-08-06 04:05:52 -0500

    chore: enable scheduling for TokenBlacklist cleanup (#27)

--- api/src/main/java/com/dmerc12/api/ApiApplication.java
+++ api/src/main/java/com/dmerc12/api/ApiApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * Main entry point for the Finance Tracker Spring Boot application.
@@ -16,6 +17,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
  * @see BaseEntity
  * @see SpringApplication
  */
+@EnableScheduling
 @EnableJpaAuditing
 @SpringBootApplication
 @EnableConfigurationProperties
